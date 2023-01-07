cd81dcf6013bc6a95a7c4941a2af91eb13c64b07
RJ McDermott <31133170+captray@users.noreply.github.com>
2023-01-07 14:25:49 -0500

    added tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'rowdie': ['Rowdies'],
+        'josefin': ['Josefin Sans']
+      }
+    },
+  },
+  plugins: [],
+}
